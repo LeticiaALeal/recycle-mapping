a4540a7711a3a2327ad69788c914a496a1140ffe
Letícia Leal <leticialeal@ciandt.com>
2022-06-26 19:50:57 -0300

    ajuste layout tela Detalhes

---
+++ recycle-mapping/src/componentes/rodape/Rodape.js
@@ -0,0 +1,12 @@
+import Menu from '../menu/Menu';
+import './Rodape.scss';
+
+export default function Rodape(){
+    return (
+        <footer className='footer'>
+            <div>
+
+            </div>
+        </footer>
+    )
+}
